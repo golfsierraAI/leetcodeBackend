fc2796850103694a07011679ca1c1c5f9ae0a6d1
gaurav <gsharma774962@gmail.com>
2022-03-10 18:32:40 +0530

    heroku

--- src/main/java/com/gourav/leetCodeBackend/service/LeetcodeService.java
+++ src/main/java/com/gourav/leetCodeBackend/service/LeetcodeService.java
@@ -1,11 +1,13 @@
 package com.gourav.leetCodeBackend.service;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("https://leetcodepatterns.herokuapp.com/")
 public interface LeetcodeService {
     @GetMapping("/get")
     List<?> getDataService();
