2bbd7ee4846d58af6a12314beef69bf195cfdbd9
gaurav <gsharma774962@gmail.com>
2022-03-10 18:17:14 +0530

    heroku

--- src/main/leetcode_ui/src/Store/rootReducer.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from "redux";
-import tableDataReducer from "./tableDataReducer";
-const rootReducer = combineReducers({ tableDataReducer });
-
-export default rootReducer;
